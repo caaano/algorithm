5a8c7e486801df8207fe1da3b96c6e83aead4328
Kim Jingi <106568279+caaano@users.noreply.github.com>
2024-04-10 15:03:12 +0900

    [level 0] Title: 외계행성의 나이, Time: 3.85 ms, Memory: 77.5 MB -BaekjoonHub

--- 프로그래머스/0/120834. 외계행성의 나이/외계행성의 나이.java
+++ 프로그래머스/0/120834. 외계행성의 나이/외계행성의 나이.java
@@ -3,7 +3,7 @@ import java.util.*;
 class Solution {
     public String solution(int age) {
         // '0'부터 '9'까지의 숫자에 대응하는 문자열을 배열에 저장
-        String[] mappings = {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j"};
+        String[] age962 = {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j"};
         
         String answer = "";
         String s = String.valueOf(age);
@@ -11,7 +11,7 @@ class Solution {
         
         for(int i = 0; i < arr.length; i++){
             // 각 숫자에 대응하는 문자열을 배열에서 가져와서 결과 문자열에 추가
-            answer += mappings[Integer.parseInt(arr[i])];
+            answer += age962[Integer.parseInt(arr[i])];
         }
         
         return answer;
