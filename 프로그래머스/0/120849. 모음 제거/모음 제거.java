1ad54d2d2c20c67a3a3ea7417038e876351ca45a
Kim Jingi <106568279+caaano@users.noreply.github.com>
2024-04-11 17:05:03 +0900

    [level 0] Title: 모음 제거, Time: 0.04 ms, Memory: 75.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/120849. 모음 제거/모음 제거.java
@@ -0,0 +1,11 @@
+class Solution {
+    public String solution(String my_string) {
+        String answer = my_string;
+        String[] arr = {"a", "e", "i", "o", "u"};
+        
+        for(String s : arr){
+            answer = answer.replace(s, "");
+        }
+        return answer;
+    }
+}
