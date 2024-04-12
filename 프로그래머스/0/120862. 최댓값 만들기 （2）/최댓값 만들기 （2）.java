635dceb2387f24091955ca7ef16967d0a5a41c7c
Kim Jingi <106568279+caaano@users.noreply.github.com>
2024-04-12 15:18:17 +0900

    [level 0] Title: 최댓값 만들기 (2), Time: 0.37 ms, Memory: 70.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] numbers) {
+        Arrays.sort(numbers);
+        
+        if(numbers[numbers.length - 1] * numbers[numbers.length - 2] > numbers[0] * numbers[1]){
+            return numbers[numbers.length - 1] * numbers[numbers.length - 2];
+        }
+        else{
+            return numbers[0] * numbers[1];
+        }
+    }
+}
