84d16275daa7db13a2c0b5bfbfe74e4edb5d6721
Kim Jingi <106568279+caaano@users.noreply.github.com>
2024-04-30 15:04:10 +0900

    [level 0] Title: 피자 나눠 먹기 (2), Time: 0.02 ms, Memory: 74.9 MB -BaekjoonHub

--- 프로그래머스/0/120815. 피자 나눠 먹기 （2）/피자 나눠 먹기 （2）.java
+++ 프로그래머스/0/120815. 피자 나눠 먹기 （2）/피자 나눠 먹기 （2）.java
@@ -2,9 +2,10 @@ class Solution {
     public int solution(int n) {
         int answer = 0;
         
-        for(int i = 1; i <= 6 * n ; i ++){
+        for(int i = 1; i <= n * 6; i ++){
             if(i * 6 % n == 0){
                 answer = i;
+                
                 break;
             }
         }
