17cb5c3ace58f312aaead9f89ef415a3f1330579
Kim Jingi <106568279+caaano@users.noreply.github.com>
2024-05-23 10:58:14 +0900

    [level 0] Title: 가까운 수, Time: 0.03 ms, Memory: 73.9 MB -BaekjoonHub

--- 프로그래머스/0/120890. 가까운 수/가까운 수.java
+++ 프로그래머스/0/120890. 가까운 수/가까운 수.java
@@ -1,16 +1,20 @@
-import java.util.*;
-
 class Solution {
     public int solution(int[] array, int n) {
         int answer = 0;
-        Arrays.sort(array);
+        int diff = 0;
+        int min = Integer.MAX_VALUE;
         
-        for(int i = 0; i < array.length; i++){
-            if(Math.abs(n - array[0]) > Math.abs(n - array[i])){
-                array[0] = array[i];
+        for (int i = 0; i < array.length; i++) {
+            diff = Math.abs(n - array[i]);
+            if (diff < min) {
+                min = diff;
+                answer = array[i];
+                
+            } else if (diff == min && array[i] < answer) {
+                answer = array[i];
             }
         }
-        answer = array[0];
+        
         return answer;
     }
 }
